491045373c92d889df7942d590e6e75b81d83c82
mykim112 <minyk112@gmail.com>
2021-10-13 18:29:58 +0900

    DTO 추가

--- src/cafekiosk/domain/CafeDTO.java
+++ src/cafekiosk/domain/CafeDTO.java
@@ -1,5 +1,19 @@
 package cafekiosk.domain;
 
-public class CafeDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CafeDTO {
+	private int no;
+	private String name;
+	private int price; 
+	private String type;
 }
--- src/cafekiosk/domain/UserDTO.java
+++ src/cafekiosk/domain/UserDTO.java
@@ -1,5 +1,19 @@
 package cafekiosk.domain;
 
-public class UserDTO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UserDTO {
+	private String tel;
+	private String name;
+	private String nickname;
+	private int point;
 }
