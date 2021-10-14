4ebed5ebedec335f8687253bf8d8bf8af024179c
Universe <tmdwnchlrh3@gmail.com>
2021-10-14 16:29:13 +0900

    CafeMenu 코드수정

--- src/cafekiosk/persistence/CafeMenu.java
+++ src/cafekiosk/persistence/CafeMenu.java
@@ -17,6 +17,8 @@ import javax.swing.JLabel;
 import java.awt.Color;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class CafeMenu extends JFrame {
 
@@ -24,24 +26,8 @@ public class CafeMenu extends JFrame {
 	private JButton btnCoffee,btnAde,btnTea,btnDessert,btnEtc;
 	private JScrollPane scrollPane;
 	private JPanel panel_1;
-	private JButton btnNewButton;
-	private JButton btnNewButton_1;
-	private JButton btnNewButton_2;
-	private JButton btnNewButton_3;
-	private JButton btnNewButton_4;
-	private JButton btnNewButton_5;
-	private JButton btnNewButton_6;
-	private JButton btnNewButton_7;
-	private JButton btnNewButton_8;
-	private JButton btnNewButton_9;
-	private JButton btnNewButton_10;
-	private JButton btnNewButton_11;
-	private JButton btnNewButton_12;
-	private JButton btnNewButton_13;
-	private JButton btnNewButton_14;
-	/**
-	 * Launch the application.
-	 */
+	private JButton btnGrapeAde,btnGrapefruitsAde,btnLemonAde;
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -64,85 +50,74 @@ public class CafeMenu extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel = new JPanel();
+		JPanel panel = new JPanel(); // 메뉴창 틀(패널)
+		contentPane.add(panel,BorderLayout.NORTH);
 		panel.setLayout(new FlowLayout(FlowLayout.LEFT,1,1));
-		contentPane.add(panel, BorderLayout.NORTH);
 		
-		 btnCoffee = new JButton("\uCEE4\uD53C");
-		panel.add(btnCoffee);
+		 btnCoffee = new JButton("커피"); // 커피 메뉴
+		 btnCoffee.addActionListener(new ActionListener() {
+		 	public void actionPerformed(ActionEvent e) {
+		 	}
+		 });
+		panel.add(btnCoffee);// 메뉴창 패널에 추가 
 		
-		btnAde = new JButton("\uC5D0\uC774\uB4DC");
+		btnAde = new JButton("에이드"); // 에이드 메뉴 
+		btnAde.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
 		panel.add(btnAde);
 		
-		 btnTea = new JButton("\uD2F0");
+		 btnTea = new JButton("티");
 		panel.add(btnTea);
 		
-		 btnDessert = new JButton("\uB514\uC800\uD2B8");
+		 btnDessert = new JButton("디저트");
 		panel.add(btnDessert);
 		
-		 btnEtc = new JButton("\uAE30\uD0C0");
+		 btnEtc = new JButton("기타");
 		panel.add(btnEtc);
 		
-		scrollPane = new JScrollPane();
-		contentPane.add(scrollPane, BorderLayout.CENTER);
+		scrollPane = new JScrollPane();// 메뉴 창 띄우는 패널
+		contentPane.add(scrollPane);
+		scrollPane.setViewportView(adeMenuPanel()); 
 		
-		panel_1 = new JPanel();
-		scrollPane.setViewportView(panel_1);
-		panel_1.setLayout(new GridLayout(0, 3, 0, 0));
-		panel_1.setBounds(0, 0, 200, 350);
+//		panel_1 = new JPanel();
+//		panel_1.setLayout(new GridLayout(0, 3, 0, 0));
+//		panel_1.setBounds(0, 0, 200, 350);
 		
-		btnNewButton = new JButton("");
-		btnNewButton.setIcon(new ImageIcon(CafeMenu.class.getResource("/image/grapeade.png")));
-		panel_1.add(btnNewButton);
-		
-		btnNewButton_1 = new JButton("");
-		btnNewButton_1.setIcon(new ImageIcon(CafeMenu.class.getResource("/image/grapefruitade.png")));
-		panel_1.add(btnNewButton_1);
-		
-		btnNewButton_2 = new JButton("");
-		btnNewButton_2.setIcon(new ImageIcon(CafeMenu.class.getResource("/image/lemon.png")));
-		panel_1.add(btnNewButton_2);
-		
-		btnNewButton_3 = new JButton("New button");
-		panel_1.add(btnNewButton_3);
-		
-		btnNewButton_4 = new JButton("New button");
-		panel_1.add(btnNewButton_4);
-		
-		btnNewButton_5 = new JButton("New button");
-		panel_1.add(btnNewButton_5);
-		
-		btnNewButton_6 = new JButton("New button");
-		panel_1.add(btnNewButton_6);
-		
-		btnNewButton_7 = new JButton("New button");
-		panel_1.add(btnNewButton_7);
-		
-		btnNewButton_8 = new JButton("New button");
-		panel_1.add(btnNewButton_8);
-		
-		btnNewButton_9 = new JButton("New button");
-		panel_1.add(btnNewButton_9);
-		
-		btnNewButton_10 = new JButton("New button");
-		panel_1.add(btnNewButton_10);
-		
-		btnNewButton_12 = new JButton("New button");
-		panel_1.add(btnNewButton_12);
-		
-		btnNewButton_11 = new JButton("New button");
-		panel_1.add(btnNewButton_11);
-		
-		btnNewButton_13 = new JButton("New button");
-		panel_1.add(btnNewButton_13);
-		
-		btnNewButton_14 = new JButton("New button");
-		panel_1.add(btnNewButton_14);
+//		btnGrapeAde = new JButton("");
+//		btnGrapeAde.setIcon(new ImageIcon(CafeMenu.class.getResource("/image/grapeade.png")));
+//		panel_1.add(btnGrapeAde);
+//		
+//		btnGrapefruitsAde = new JButton("");
+//		btnGrapefruitsAde.setIcon(new ImageIcon(CafeMenu.class.getResource("/image/grapefruitade.png")));
+//		panel_1.add(btnGrapefruitsAde);
+//		
+//		btnLemonAde = new JButton("");
+//		btnLemonAde.setIcon(new ImageIcon(CafeMenu.class.getResource("/image/lemon.png")));
+//		panel_1.add(btnLemonAde);
 		setSize(500,300);
 		
+	}
+	public JPanel adeMenuPanel() {
+		panel_1 = new JPanel();
+		panel_1.setLayout(new GridLayout(0, 3, 0, 0));
+		panel_1.setBounds(0, 0, 200, 350);
+		btnGrapeAde = new JButton("");
+		btnGrapeAde.setIcon(new ImageIcon(CafeMenu.class.getResource("/image/grapeade.png")));
+		panel_1.add(btnGrapeAde);
+		
+		btnGrapefruitsAde = new JButton("");
+		btnGrapefruitsAde.setIcon(new ImageIcon(CafeMenu.class.getResource("/image/grapefruitade.png")));
+		panel_1.add(btnGrapefruitsAde);
+		
+		btnLemonAde = new JButton("");
+		btnLemonAde.setIcon(new ImageIcon(CafeMenu.class.getResource("/image/lemon.png")));
+		panel_1.add(btnLemonAde);
+		return panel_1;
 	}
 	
 	
